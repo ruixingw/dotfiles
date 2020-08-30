9230b5edb296c0f1880efb068de5355d26b63a71
ruixingw <ruixingw@outlook.com>
2020-08-30 21:14:34 +0800

    x11 forwarding for wsl2; xtb update

--- apps.d/xtb.sh
+++ apps.d/xtb.sh
@@ -2,4 +2,4 @@ nproc=`grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}'`
 export OMP_NUM_THREADS=$nproc
 export MKL_NUM_THREADS=$nproc
 export XTBHOME=$HOME/apps/xtb
-source $XTBHOME/Config_xtb_env.bash
+source $XTBHOME/share/xtb/config_env.bash
--- zshrc
+++ zshrc
@@ -26,3 +26,4 @@ if test -d $HOME/.apps.d/; then
 fi
 export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
 export LIBGL_ALWAYS_INDIRECT=1
+
